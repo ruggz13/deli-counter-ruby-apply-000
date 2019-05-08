2c76555f8e31b3a49704f16350fe4a28509991ef
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-08 16:04:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,12 @@ katz_deli = []
 def line(katz_deli)
   if (!katz_deli.length)
     puts "The line is currently empty."
+    numbersAndNames = []
+    counter = 0
+    len = katz_deli.length
+    until counter == len
+      counter += 1
+      numbersAndNames.push("#{counter+1}. #{katz_deli[counter]}")
+      puts "The line is currently: #{numbersAndNames.join(", ")}"
   end
 end
