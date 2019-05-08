5f73987a83ceccbe94dbee535408c95576a17268
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-08 16:29:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
