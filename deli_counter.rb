994d3a53d4f520417d7366421ef62838eebd875b
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-08 16:07:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,16 @@ def line(katz_deli)
       puts "The line is currently: #{numbersAndNames.join(", ")}"
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome #{name}. You are #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if (!katz_deli.length)
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift()}."
+  end
+end 
